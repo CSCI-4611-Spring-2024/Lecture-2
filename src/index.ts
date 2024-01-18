1f67327d40469c03b1b21256856a1cca9f59e665
Evan Suma Rosenberg <suma@umn.edu>
2024-01-18 13:10:02 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* Lecture 2
+ * CSCI 4611, Spring 2023, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { App } from './App'
+
+const app = new App();
+app.start();
